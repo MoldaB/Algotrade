9a5aa69442acebaca2ac7f414cd29a297970bf62
Yevgeny Beygel <hbeygel@gmail.com>
2017-05-09 23:03:03 +0300

    improved functions for percentages

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 import sys
 import os
-from pandas import *
+import pandas as pd
 import numpy as nm
 
 path = os.getcwd()
@@ -14,17 +14,17 @@ orders_set = None
 
 new_features = ['avr_price'
                 , 'max_price'
-                , 'avr_volium'
-                , 'max_volium'
-                , 'std_volium'
-                , 'partial_vol_percent'
+                , 'avr_volume'
+                , 'max_volume'
+                , 'std_volume'
                 , 'QGFlag'
                 , 'percent_canceled'
                 , 'percent_updated'
                 , 'percent_Completed'
-                , 'IOC//None'
                 , 'buy_percent'
                 , 'sell_percent'
+                , 'IOC//None'
+                , 'partial_vol_percent'
                 , 'avr_num_of_Updates_per_order'
                 , 'max_num_of_Updates_per_order'
                 , 'std_num_of_Updates_per_order'
@@ -45,26 +45,31 @@ new_features = ['avr_price'
                 , 'activity_hours_15_to_21'
                 , 'activity_hours_22_to_03'
                 , 'activity_hours_04_to_08']
-new_df = pandas.DataFrame
+new_df = pd.DataFrame
 
 try:
-    file_abs_path = os.path.abspath(os.path.join(os.getcwd(), "orders.csv"))
+    file_abs_path = os.path.abspath(os.path.join(os.getcwd(), "orders_1.csv"))
     with open(file_abs_path, 'r') as f:
-        orders_set = pandas.read_csv(f)
+        orders_set = pd.read_csv(f)
 
         f = {'Price': {'avr_price': 'mean', 'max_price': 'max'}, 'Vol': {'avr_volume': 'mean', 'max_volume': 'max', 'std_volume': 'std'}, 'QGFlag': {'QG/None': 'count'}}
-        grouped1 = orders_set.groupby(['Id']).agg(f)
-        new_df1 = grouped1.reset_index()
+        grouped1 = orders_set.groupby(['Id']).agg(f, axis=1)
 
-        canceled_df = orders_set[(orders_set['Status'] == 'X')].groupby('Id').agg({'Status': {'canceled': 'count'}})
-        updated_df = orders_set[(orders_set['Status'] == 'O')].groupby('Id').agg({'Status': {'updated': 'count'}})
-        completed_df = orders_set[(orders_set['Status'] == 'M')].groupby('Id').agg({'Status': {'completed': 'count'}})
+        grouped_status= pd.get_dummies(orders_set['Status'], prefix='status')\
+            .groupby(orders_set['Id'])\
+            .sum() \
+            .apply( lambda x: x / x.sum() * 100, axis=1)
 
-        new_df = pandas.merge(canceled_df.reset_index(),updated_df.reset_index(), on='Id', how='outer').merge(completed_df.reset_index(), on='Id', how='outer').merge(new_df1, on='Id', how='outer')
+        grouped_buy_sell = pd.get_dummies(orders_set['Side'], prefix='side')\
+            .groupby(orders_set['Id'])\
+            .sum()\
+            .apply(lambda x: x / x.sum() * 100, axis=1)
+
+        print("finished building all features")
+        new_df = grouped1.join(grouped_status, how='outer')\
+            .join(grouped_buy_sell, how='outer')
 
         new_df.to_csv('new_df.csv')
-
-        print("bla bla")
 
 except Exception as inst:
     print(inst.args)
